e4b8f57c69297829da5659d63a37aceee7737cba
lewisc402 <lewisc402@gmail.com>
2018-01-17 18:07:23 +0800

    add Dnd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
-import {render} from "preact";
-import Lane from "./Lane"
-import 'antd/dist/antd.css';
+import { render } from "preact";
+import Lane from "./lane";
+import "antd/dist/antd.css";
 import { Card, Button } from "antd";
 
 render(
-  <div style={{ background: '#ECECEC', padding: '30px' }}>    
-    <Lane>
-    </Lane>
-  </div>
-  , document.getElementById('root'));
+  <div style={{ background: "#ECECEC", padding: "30px" }}>
+    <Lane />
+  </div>,
+  document.getElementById("root")
+);
